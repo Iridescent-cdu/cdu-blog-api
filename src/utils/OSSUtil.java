ddf6da3912a75c2c6b51a44a07dd7dd6ed48f169
Iridescent <73779267+Iridescent-cdu@users.noreply.github.com>
2023-01-05 17:43:03 +0800

    2023-1-4

--- src/utils/OSSUtil.java
+++ src/utils/OSSUtil.java
@@ -8,13 +8,13 @@ public class OSSUtil {
     //地域节点
     private static String ENDPOINT = "oss-cn-chengdu.aliyuncs.com";
     //访问ID
-    private static String ACCESSKEYID = "LTAI5tFqr9PbQqndaAZkC7bM";
+    private static String ACCESSKEYID = "";
     //访问秘钥
-    private static String ACCESSKEYSECRET = "cC8txWnTQ2mCafut9vDQpcSg9Hntai";
+    private static String ACCESSKEYSECRET = "";
     //仓库名称
     private static String BUCKETNAME = "cdu-blog-api";
     //外网域名
-    private static String SUFFER_URL = "https://cdu-blog-api.oss-cn-chengdu.aliyuncs.com/";
+    private static String SUFFER_URL = "";
 
     /**
      * 获取OSS链接
